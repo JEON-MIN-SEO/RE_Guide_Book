5cecd29eb3226a0b8b4e56d8cd986f438f978114
jmsparrowk <jmsparrowk@naver.com>
2024-09-04 17:43:50 +0900

    9/4

---
+++ src/main/java/guide_book_2/KTO_public_api_2/KtoPublicApi2Application.java
@@ -0,0 +1,13 @@
+package guide_book_2.KTO_public_api_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtoPublicApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtoPublicApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guide_book_2/KTO_public_api_2/KtoPublicApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guide_book_2.KTO_public_api_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtoPublicApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
